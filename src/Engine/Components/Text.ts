fe834893086aa9f3b6fea2ecbdd10c98f47581be
Christoffer <snapgravy@gmail.com>
2019-08-22 18:17:33 +0900

    floating toolbar

--- src/Engine/Components/Text.ts
+++ src/Engine/Components/Text.ts
@@ -43,7 +43,28 @@ export class Text extends Component {
   private loadQuill() {
     this.quill = new this.page.Quill(this.element, {
       theme: "bubble",
-      placeholder: "Compose an epic..."
+      placeholder: "Compose an epic...",
+      modules: {
+        toolbar: [
+          ["bold", "italic", "underline", "strike"], // toggled buttons
+          ["blockquote", "code-block"],
+
+          [{ header: 1 }, { header: 2 }], // custom button values
+          [{ list: "ordered" }, { list: "bullet" }],
+          [{ script: "sub" }, { script: "super" }], // superscript/subscript
+          [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+          [{ direction: "rtl" }], // text direction
+
+          [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+          [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+          [{ font: [] }],
+          [{ align: [] }],
+
+          ["clean"]
+        ]
+      }
     });
 
     // ### Selection Change
