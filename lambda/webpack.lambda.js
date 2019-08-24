69e94f4bb1350b5fafea55d4f7621ccc6744bd99
mattgi <matt.ginty@gmail.com>
2019-08-24 20:31:04 +1000

    maybe

--- lambda/webpack.lambda.js
+++ lambda/webpack.lambda.js
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
 require("dotenv").config({ path: __dirname + "/../.env" });
 
 const envKeys = [];
 
 module.exports = {
   mode: "development",
+  target: "node", // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()],
   plugins: [
     new webpack.DefinePlugin(
       envKeys.reduce(
