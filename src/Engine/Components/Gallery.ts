391a8e885e7a556fa17b2c1fc0e61dd14b60eec8
mattgi <matt.ginty@gmail.com>
2019-08-29 22:55:46 +1000

    Revert "amp.- revert me"
    This reverts commit f029c44c6e5891e98d1055ef920353fd822fd8ce.

--- src/Engine/Components/Gallery.ts
+++ src/Engine/Components/Gallery.ts
@@ -5,36 +5,30 @@ import { Stack } from "../Stack";
 import { setStyle } from "../Utils";
 
 export class Gallery extends Component {
-  public element: HTMLElement;
+  public element: HTMLDivElement;
 
   constructor(stack: Stack, data: any) {
     super(stack, data);
 
-    this.element = document.createElement("amp-carousel");
+    this.area.append((this.element = document.createElement("div")));
     this.element.id = data.id;
-    this.element.setAttribute("layout", "responsive");
-    this.element.setAttribute("type", "slides");
-    this.element.setAttribute("autoplay", "");
-    this.element.setAttribute("delay", "8000");
-    this.element.setAttribute("Width", `${viewport.width}px`);
-    this.element.setAttribute("height", `${viewport.height}px`);
 
     const items = this.getSetting("items");
+
     items.forEach((item: any, index: number) => {
-      // const itemContainer = document.createElement("div");
+      const itemContainer = document.createElement("div");
 
-      // setStyle(itemContainer, {
-      //   margin: "auto"
-      // });
+      setStyle(itemContainer, {
+        margin: "auto"
+      });
 
-      const img = document.createElement("amp-img");
-      img.setAttribute("layout", "fill");
-      // img.width = 300;
-      // img.height = 169;
-      img.setAttribute("src", item.src);
+      const img = document.createElement("img");
+      img.width = 300;
+      img.height = 169;
+      img.src = item.src;
       setStyle(img, {});
-      this.element.append(img);
-      //      this.element.append(itemContainer);
+      itemContainer.append(img);
+      this.element.append(itemContainer);
     });
 
     this.element.onclick = () => {
@@ -42,21 +36,20 @@ export class Gallery extends Component {
     };
 
     setStyle(this.element, { pointerEvents: "auto" });
-    this.area.append(this.element);
   }
 
   public render() {
     super.render();
 
-    // setStyle(this.element, {
-    //   height: viewport.height,
-    //   display: "grid",
-    //   gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-    //   gridGap: "1em",
-    //   justifyContent: "center",
-    //   minWidth: "100%",
-    //   minHeight: "100%",
-    //   ...(this.data.style || {})
-    // });
+    setStyle(this.element, {
+      height: viewport.height,
+      display: "grid",
+      gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+      gridGap: "1em",
+      justifyContent: "center",
+      minWidth: "100%",
+      minHeight: "100%",
+      ...(this.data.style || {})
+    });
   }
 }
--- src/Engine/Components/Image.ts
+++ src/Engine/Components/Image.ts
@@ -15,7 +15,7 @@ export class Image extends Component {
    * Image element.
    * @type {HTMLImageElement}
    */
-  private image: any;
+  private image: HTMLImageElement;
 
   /**
    * Scroller element, used during sticky positioning.
@@ -29,27 +29,22 @@ export class Image extends Component {
     this.area.append((this.element = document.createElement("figure")));
     this.element.id = data.id;
 
-    this.element.onclick = () => {
+    this.image = document.createElement("img");
+    this.image.onclick = () => {
       this.edit();
     };
+    this.element.append(this.image);
 
     setStyle(this.element, { pointerEvents: "auto" });
   }
 
   public render() {
     super.render();
-    if (this.element.children.length === 0) {
-      this.image = document.createElement("amp-img");
-      this.image.setAttribute(
-        "src",
-        this.getSetting("src", "https://images.unsplash.com/photo-1505207820475-ad20164d3936?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
-      );
-      this.image.title = this.getSetting("title", "");
-      this.image.alt = this.getSetting("altText", "");
-      this.image.setAttribute("layout", "fill");
-      this.image.className = "cover";
-      this.element.append(this.image);
-    }
+
+    this.image.src = this.getSetting("src");
+    this.image.title = this.getSetting("title", "");
+    this.image.alt = this.getSetting("altText", "");
+
     if (this.scroller) {
       this.scroller.remove();
       this.scroller = undefined;
@@ -73,35 +68,35 @@ export class Image extends Component {
     switch (position) {
       case "absolute": {
         this.element.className = "position-fixed";
-        // this.image.className = "";
-        // setStyle(
-        //   this.image,
-        //   {
-        //     objectFit: "cover",
-        //     objectPosition,
-        //     width: "100%",
-        //     height: "100%",
-        //     top: null
-        //   },
-        //   true
-        // );
+        this.image.className = "";
+        setStyle(
+          this.image,
+          {
+            objectFit: "cover",
+            objectPosition,
+            width: "100%",
+            height: "100%",
+            top: null
+          },
+          true
+        );
         break;
       }
 
       case "sticky": {
         this.element.className = "position-fixed";
-        // this.image.className = "position-sticky";
-        // setStyle(
-        //   this.image,
-        //   {
-        //     objectFit: "cover",
-        //     objectPosition,
-        //     width: "100%",
-        //     height: "100%",
-        //     top: null
-        //   },
-        //   true
-        // );
+        this.image.className = "position-sticky";
+        setStyle(
+          this.image,
+          {
+            objectFit: "cover",
+            objectPosition,
+            width: "100%",
+            height: "100%",
+            top: null
+          },
+          true
+        );
         break;
       }
 
@@ -115,17 +110,17 @@ export class Image extends Component {
         };
 
         this.image.className = "position-fixed_component";
-        // setStyle(
-        //   this.image,
-        //   {
-        //     objectFit: "cover",
-        //     objectPosition,
-        //     top: viewport.offset.top,
-        //     width: viewport.width,
-        //     height: viewport.height
-        //   },
-        //   true
-        // );
+        setStyle(
+          this.image,
+          {
+            objectFit: "cover",
+            objectPosition,
+            top: viewport.offset.top,
+            width: viewport.width,
+            height: viewport.height
+          },
+          true
+        );
 
         if (!this.scroller) {
           this.scroller = scroller;
@@ -137,17 +132,17 @@ export class Image extends Component {
 
       default: {
         this.element.className = "";
-        // this.image.className = "";
-        // setStyle(
-        //   this.image,
-        //   {
-        //     display: "block",
-        //     width: viewport.width,
-        //     ...(this.data.style || {}),
-        //     top: null
-        //   },
-        //   true
-        // );
+        this.image.className = "";
+        setStyle(
+          this.image,
+          {
+            display: "block",
+            width: viewport.width,
+            ...(this.data.style || {}),
+            top: null
+          },
+          true
+        );
       }
     }
   }
